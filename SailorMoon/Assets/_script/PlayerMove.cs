bfbda571bc8e06e56365d832b65d9642df74e644
Sun Jing <1808486675@qq.com>
2018-12-20 21:19:23 +0800

    人物由静止到走的移动

--- SailorMoon/Assets/_script/PlayerMove.cs
+++ SailorMoon/Assets/_script/PlayerMove.cs
@@ -13,14 +13,18 @@ public class PlayerMove : MonoBehaviour
 
     #endregion
     #region Public 变量
-
+    public Animator playerAnimator;//角色移动状态机
+    public float playerSpeed;//人物速度
+    float distance;//距离
     #endregion
 
     #region Public 方法
     public void Update()
     {
+        playerSpeed = 2f;
+        //跟随鼠标移动
         if (Input.GetMouseButtonDown(0))
-        {
+        {          
             //从鼠标当前位置发射一条射线
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             //声明保存射线检测结果的变量
@@ -33,10 +37,18 @@ public class PlayerMove : MonoBehaviour
                     return;
                 //获取射线与地面的碰撞点
                 Vector3 targetPos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                distance = Vector3.Distance(transform.position,targetPos);//距离目标点的距离
+                transform.DOLookAt(hit.point,1f);//朝向
                 //Player向目标位置进行移动
-                transform.DOMove(targetPos, 1f);
+                playerAnimator.SetFloat("speed",playerSpeed);
+                transform.DOMove(targetPos, (distance/playerSpeed)).OnComplete(StopMove);              
             }
         }
+    }
+    public void StopMove()
+    {
+        playerSpeed = 0f;
+        playerAnimator.SetFloat("speed", playerSpeed);
     }
     #endregion
     #region Private 方法
