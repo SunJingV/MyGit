356b1f892eae255aad9349849780e587431475fd
Sun Jing <1808486675@qq.com>
2018-12-28 10:01:20 +0800

    磨床的移动

---
+++ SailorMoon/Assets/_script/ButtonType.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonType : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+{
+    #region Private 变量
+    // 延迟时间
+    private float delay = 0.1f;
+    // 按钮是否是按下状态
+    private bool isDown = false;
+    // 按钮最后一次是被按住状态时候的时间
+    private float lastIsDownTime;
+    #endregion
+    #region Protected 变量
+
+    #endregion
+    #region Public 变量
+    public ButtonDownType buttonType=ButtonDownType.None;//按钮对应的类型   
+    #endregion
+
+    #region Public 方法
+    //根据类型调用不同的方法
+ public void SwitchBtn()
+    {
+        switch (buttonType)
+        {
+            case ButtonDownType.None:
+                break;
+            case ButtonDownType.girindingLeft:
+                GrinderMove.Instance.LeftGrinding();
+                break;
+            case ButtonDownType.grindingShelfUp:
+                GrinderMove.Instance.GrindingShelfUp();
+                break;
+            case ButtonDownType.grindingShelfDown:
+                GrinderMove.Instance.GrindingShelfDown();
+                break;
+            case ButtonDownType.workSpaceLeft:
+                GrinderMove.Instance.WorkSpaceLeft();
+                break;
+            case ButtonDownType.workSpaceLeftMax:
+                GrinderMove.Instance.WorkSpaceLeftMax();
+                break;
+            case ButtonDownType.workSpaceRight:
+                GrinderMove.Instance.WorkSpaceRight();
+                break;
+            default:
+                break;
+        }
+    }
+public void OnPointerDown(PointerEventData eventData)
+    {
+        isDown = true;
+        lastIsDownTime = Time.time;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isDown = false;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isDown = false;
+    }
+    #endregion
+    #region Private 方法
+    private void Update()
+    {
+        if (isDown)
+        {
+            // 当前时间 -  按钮最后一次被按下的时间 > 延迟时间0.2秒
+            if (Time.time - lastIsDownTime > delay)
+            {
+                SwitchBtn();
+                // 触发长按方法
+                // Debug.Log("长按");
+                //movement.Istance.DownButton();
+                // 记录按钮最后一次被按下的时间
+                lastIsDownTime = Time.time;
+
+            }
+        }
+    }
+}
+    #endregion
+    #region Protected 方法
+
+    #endregion
+public enum ButtonDownType
+{
+    None=0,
+    girindingLeft,//砂轮左移
+ grindingShelfUp,//砂轮架上移按钮
+ grindingShelfDown,//砂轮架下移按钮
+ workSpaceLeft,//工作台左移按钮
+ workSpaceLeftMax,//工作台左移按钮
+ workSpaceRight//工作台左移按钮
+}
---
+++ SailorMoon/Assets/_script/GrinderMove.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+/// <summary>
+/// 磨床移动类
+/// </summary>
+public class GrinderMove : MonoBehaviour
+{
+    #region Private 变量
+
+    #endregion
+    #region Protected 变量
+
+    #endregion
+    #region Public 变量
+    public GameObject grindingWheel;//砂轮
+    public GameObject grindingShelf;// 砂轮架
+    public GameObject workSpace;//工作台
+    public Transform grindingLeftMax;//左移最大限度
+    public Transform grindingShelfDownMax;
+    public Transform grindingShelfUpMax;
+    public Transform workSpaceLeftMax;
+    public Transform workSpaceRightMax;
+    private static GrinderMove instance;
+    public static GrinderMove Instance
+    {
+        get
+        {
+            return instance;
+        }
+
+        set
+        {
+            instance = value;
+        }
+    }
+    Vector3 vector = new Vector3(0.02f,0,0);    //砂轮每次移动的距离
+    Vector3 vector1 = new Vector3(0,0.02f,0);
+    Vector3 vector2 = new Vector3(0.02f,0,0);
+    #endregion
+
+    #region Public 方法
+    //砂轮匀速转动
+    public void GrindingWheelRotate()
+    {
+        grindingWheel.transform.Rotate(transform.forward * 60f * Time.deltaTime);
+    }
+    //砂轮左移
+    public void LeftGrinding()
+    {//左移限度
+        if (grindingWheel.transform.position.x< grindingLeftMax.position.x)
+        {
+              grindingWheel.transform.Translate(vector,0);
+        }
+        else
+        {
+            grindingWheel.transform.Translate(Vector3.zero, 0);
+        }
+       
+    }
+    //砂轮架上移
+    public void GrindingShelfUp()
+    {
+        if (grindingShelf.transform.position.y< grindingShelfUpMax.position.y)
+        {
+          grindingShelf.transform.Translate(vector1,0);
+        }
+        else
+        {
+            grindingShelf.transform.Translate(Vector3.zero, 0);
+        }
+    }
+    //砂轮架下移
+    public void GrindingShelfDown()
+    {
+        if (grindingShelf.transform.position.y > grindingShelfDownMax.position.y)
+        {
+            grindingShelf.transform.Translate(-vector1, 0);
+        }
+        else
+        {
+            grindingShelf.transform.Translate(Vector3.zero, 0);
+        }
+    }
+    //工作空间移到最左侧
+    public void WorkSpaceLeftMax()
+    {
+        workSpace.transform.DOMoveX(workSpaceLeftMax.position.x,1f);
+    }
+    //工作台左移
+    public void WorkSpaceLeft()
+    {
+        if (workSpace.transform.position.x< workSpaceLeftMax.position.x)
+        {
+         workSpace.transform.Translate(vector2,0);
+        }
+        else
+        {
+            workSpace.transform.Translate(Vector3.zero, 0);
+        }
+    }
+public   void WorkSpaceRight()
+    {
+        if (workSpace.transform.position.x > workSpaceRightMax.position.x)
+        {
+            workSpace.transform.Translate(-vector2, 0);
+        }
+        else
+        {
+            workSpace.transform.Translate(Vector3.zero, 0);
+        }
+    }
+    #endregion
+    #region Private 方法
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Update()
+    {
+        GrindingWheelRotate();
+    }
+
+    #endregion
+    #region Protected 方法
+
+    #endregion
+
+}
